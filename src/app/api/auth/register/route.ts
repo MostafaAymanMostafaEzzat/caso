43a4cf2d8484b51d9ed12572817f10252e63005b
MostafaAymanMostafaEzzat <es-Moustafa.Ezzat2026@alexu.edu.eg>
2024-10-26 12:41:23 +0300

    check14979942643463

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -7,7 +7,7 @@ import { Resend } from "resend";
 import { NextRequest, NextResponse } from "next/server";
 const bcrypt = require('bcrypt');
 
-export async function GET(req:Request) :Promise<Response>{
+export async function POST(req:Request) :Promise<Response>{
   let i= 0
 
   console.log(i++)
