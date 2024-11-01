f33181b6bf0b8321d0f5e1d867ebe8e56d698935
MostafaAymanMostafaEzzat <es-Moustafa.Ezzat2026@alexu.edu.eg>
2024-11-01 09:46:01 +0200

    checkk21

--- src/app/api/webHook/router.ts
+++ src/app/api/webHook/router.ts
@@ -6,28 +6,38 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST(request: Request) {
-
+console.log('start webhook')
 
     const result = await request.text();
+console.log('start webhook 1')
+
     // Only verify the event if you have an endpoint secret defined.
     // Otherwise use the basic event deserialized with JSON.parse
 
       const signature = request.headers.get('stripe-signature');
+console.log('start webhook 2')
+
       if (!signature) {
         return new Response('Invalid signature', { status: 400 })
       }
+console.log('start webhook 3')
   
   let event;
 
   try {
     event = stripe.webhooks.constructEvent(result, signature,  process.env.STRIPE_WEBHOOK_SECRET!);
  
+    console.log('start webhook 4')
 
   
 if(event.type === 'checkout.session.completed'){
     if(!event.data.object.customer_details?.email){
+    console.log('start webhook 5')
+
         throw new Error("Email is missing")
+
     }
+    console.log('start webhook 6')
 
     const session = event.data.object
     const {userId,orderId} =session.metadata || {
@@ -39,6 +49,7 @@ if(event.type === 'checkout.session.completed'){
       }
       const billingAddress = session.customer_details?.address
       const ShippingAddress = session.shipping_details?.address
+      console.log('start webhook 7')
 
       const updateOrder = await db.order.update({
         where:{
@@ -69,26 +80,28 @@ if(event.type === 'checkout.session.completed'){
               },
         }
       })
-      const { data, error } = await resend.emails.send({
-        from: 'caso <es-moustafa.ezzat2026@alexu.edu.eg>',
-        to: [event.data.object.customer_details.email],
-        subject: 'Thanks for your order!',
-        react: OrderReceivedEmail({
-            orderId,
-            orderDate: updateOrder.createdAt.toLocaleDateString(),
-            // @ts-ignore
-            shippingAddress: {
-              name: session.customer_details!.name!,
-              city: ShippingAddress!.city!,
-              country: ShippingAddress!.country!,
-              postalCode: ShippingAddress!.postal_code!,
-              street: ShippingAddress!.line1!,
-              state: ShippingAddress!.state,
-            },
-          }),
+      console.log('start webhook 8')
+
+      // const { data, error } = await resend.emails.send({
+      //   from: 'caso <es-moustafa.ezzat2026@alexu.edu.eg>',
+      //   to: [event.data.object.customer_details.email],
+      //   subject: 'Thanks for your order!',
+      //   react: OrderReceivedEmail({
+      //       orderId,
+      //       orderDate: updateOrder.createdAt.toLocaleDateString(),
+      //       // @ts-ignore
+      //       shippingAddress: {
+      //         name: session.customer_details!.name!,
+      //         city: ShippingAddress!.city!,
+      //         country: ShippingAddress!.country!,
+      //         postalCode: ShippingAddress!.postal_code!,
+      //         street: ShippingAddress!.line1!,
+      //         state: ShippingAddress!.state,
+      //       },
+      //     }),
 
 
-        })
+      //   })
   
         return NextResponse.json({ result: event, ok: true })
 }}  catch (err) {
--- src/app/configure/preview/action.ts
+++ src/app/configure/preview/action.ts
@@ -86,6 +86,7 @@ export default async function creatSession({ ConfigID }: { ConfigID: string }) {
     cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}`,
   })
   console.log('end session')
+  console.log(session.url)
 
   return { url: session.url }
 }
