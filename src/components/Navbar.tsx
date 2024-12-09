0f95742a27b72018691a4716e4bf79aa6146ef3f
MostafaAymanMostafaEzzat <es-Moustafa.Ezzat2026@alexu.edu.eg>
2024-12-09 08:30:17 +0200

    12/823324695

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,34 +17,34 @@ const [auth,setAuth] = useState<null | { userId: string; role: string;}>(null);
 const x = useRef(true)
 console.log(x)
 
-  useEffect(()=>{
+//   useEffect(()=>{
 
-  async function authenticate (){
-    try {
-      const user = await axios.get(
-        `${process.env.NEXT_PUBLIC_SERVER_URL}/api/auth/authenticateUser`);
-        console.log("user")
+//   async function authenticate (){
+//     try {
+//       const user = await axios.get(
+//         `${process.env.NEXT_PUBLIC_SERVER_URL}/api/auth/authenticateUser`);
+//         console.log("user")
   
-        console.log(user.data.user)
-        if (JSON.stringify(user.data.user) !== JSON.stringify(auth)) {
-          setAuth(user.data.user);
-          x.current = false;
-          console.log("Authenticated user:", user);
-        }
-    } catch (error) {
-      console.error("Authentication error");
-      if (auth !== null) {
-        setAuth(null); // Clear state if there's an error
-        x.current = false;
+//         console.log(user.data.user)
+//         if (JSON.stringify(user.data.user) !== JSON.stringify(auth)) {
+//           setAuth(user.data.user);
+//           x.current = false;
+//           console.log("Authenticated user:", user);
+//         }
+//     } catch (error) {
+//       console.error("Authentication error");
+//       if (auth !== null) {
+//         setAuth(null); // Clear state if there's an error
+//         x.current = false;
 
-      }
-    }
-  }
-  if(x.current){
-    console.log("if(x){}"); console.log(x)
-  authenticate();
-}else{ console.log("x.current"); console.log(x);x.current= true}
-  })
+//       }
+//     }
+//   }
+//   if(x.current){
+//     console.log("if(x){}"); console.log(x)
+//   authenticate();
+// }else{ console.log("x.current"); console.log(x);x.current= true}
+//   })
 
 
 
