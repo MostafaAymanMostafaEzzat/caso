111dd19a8f9d7656737a5991318d61a91958090c
MostafaAymanMostafaEzzat <es-Moustafa.Ezzat2026@alexu.edu.eg>
2024-12-12 11:59:50 +0200

    final5

--- src/utils/sendVerficationEmail.ts
+++ src/utils/sendVerficationEmail.ts
@@ -1,6 +1,8 @@
 import {sendEmail} from './sendEmail';
 
 export const sendVerificationEmail = async ({email,verificationToken,origin}:{email:string,verificationToken:string,origin:string})=>{
+console.log('email')
+console.log(email)
 
 const URL =`${origin}/auth/verify-email?verificationToken=${verificationToken}&email=${email}`
 const message=`<p>Please confirm your email by clicking on the following link : 
